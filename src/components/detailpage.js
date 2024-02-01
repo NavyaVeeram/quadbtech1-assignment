63b8d7e01d3e41505ba751e036347ccb83826f7f
NavyaVeeram <navyareddyveeram2000@gmail.com>
2024-02-01 14:52:31 +0530

    changing the details page  commit

--- src/components/detailpage.js
+++ src/components/detailpage.js
@@ -24,17 +24,20 @@ function DetailsPage() {
       <div className="container w-100">
         <div>
           {records.map((list, index) => (
-            <div key={index} className="d-flex my-4">
-              {list.show.image && list.show.image.medium && (
-                <div className="image mr-4">
-                  <img src={list.show.image.medium} alt="images" width={'100px'} height={'150px'}/>
-                </div>
-              )}
+             <div className="container bg-light p-3 d-flex">
+              <div className="image">
+             {list.show.image && list.show.image.medium ? (
+               <img src={list.show.image.medium} alt="Show" style={{ height:'200px',width:'100px',objectFit: 'cover' }} />
+             ) : (
+               <div style={{  backgroundColor: 'lightgray',height:'200px',width:'100px'}}></div>
+             )}
+             </div>
               <div className="content p-5">
                 <p style={{ fontFamily: 'poppins' }}>
                   {sanitizeHTML(list.show.summary)}
                 </p>
               </div>
+           
             </div>
           ))}
         </div>
